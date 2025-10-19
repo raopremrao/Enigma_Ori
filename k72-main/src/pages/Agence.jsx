715b54f8e4be48ca695460a566612c38caaacebe
raopremrao <raom61337@gmail.com>
2025-10-19 23:42:00 +0530

    Final changes in Agence for mobile

--- k72-main/src/pages/Agence.jsx
+++ k72-main/src/pages/Agence.jsx
@@ -77,7 +77,7 @@ const Agence = () => {
   return (
     <div className='parent'>
       <div id='page1' className='py-1 '>
-        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-70 top-85 lg:left-[15vw] left-[10vw]'>
+        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-70 top-110 lg:left-[15vw] left-[10vw]'>
           <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
         </div>
         <div className='relative font-[font2]'>
