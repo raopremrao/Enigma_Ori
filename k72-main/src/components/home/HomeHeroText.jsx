81a79dd748ef0b53f0095d1fa3e1c01546a43834
raopremrao <raom61337@gmail.com>
2025-10-19 23:25:16 +0530

    fixed home responsive for mobile

--- k72-main/src/components/home/HomeHeroText.jsx
+++ k72-main/src/components/home/HomeHeroText.jsx
@@ -3,7 +3,7 @@ import Video from './Video'
 
 const HomeHeroText = () => {
     return (
-        <div className='font-[font1]  mt-55 lg:mt-0 pt-5 text-center'>
+        <div className='font-[font1]  mt-20 lg:mt-0 pt-5 text-center'>
             <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
                 The spark for
             </div>
--- k72-main/src/pages/Home.jsx
+++ k72-main/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <div className='h-screen w-screen fixed'>
         <Video />
       </div>
-      <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
+      <div className='w-screen relative pb-5 overflow-hidden flex flex-col justify-between' style={{ height: '100dvh', overflow: 'hidden' }}>
         <HomeHeroText />
         <HomeBottomText />
       </div>
