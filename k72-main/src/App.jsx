07580826524f42795638d20c0682b7e39a24f510
raopremrao <raom61337@gmail.com>
2025-10-20 00:50:01 +0530

    Done With Lenis

--- k72-main/src/App.jsx
+++ k72-main/src/App.jsx
@@ -5,9 +5,26 @@ import Projects from './pages/Projects'
 import Navbar from './components/Navigation/Navbar'
 import FullScreenNav from './components/Navigation/FullScreenNav'
 import About from './pages/About'
+import Lenis from 'lenis'
+import { useEffect } from 'react'
+import gsap from 'gsap'; 
+import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
 
 
 const App = () => {
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on('scroll', ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+  }, []);
+  
+
   return (
     <div className='overflow-x-hidden'>
       <Navbar />
