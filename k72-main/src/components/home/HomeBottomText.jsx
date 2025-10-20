c1ad70e1d6db316e1670fd37daec9935a2880c29
raopremrao <raom61337@gmail.com>
2025-10-20 11:43:20 +0530

    Made some changes

--- k72-main/src/components/home/HomeBottomText.jsx
+++ k72-main/src/components/home/HomeBottomText.jsx
@@ -5,7 +5,7 @@ const HomeBottomText = () => {
 
   return (
     <div className='font-[font2] flex items-center justify-center gap-7 '>
-      <p className='absolute lg:w-[17vw] w-64 lg:top-90 lg:right-20 right-20 bottom-35  lg:bottom-72 font-[font1] lg:text-[1vw] text-xs lg:leading-relaxed leading-tight'>
+      <p className='absolute lg:w-[17vw] w-64 lg:top-90 lg:right-20 right-20 bottom-35  lg:bottom-72 font-sans lg:text-[1vw] font-bold text-xs lg:leading-relaxed leading-tight'>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.</p>
       <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-30 flex items-center px-4 pt-1 lg:px-14 border-white rounded-full uppercase'>
         <Link className='text-[4vw] lg:mt-6' to='/projects'>Projects</Link>
